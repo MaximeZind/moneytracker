8c2b6e4107561de155e92234c8d21bc49a07f95b
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-02-05 13:19:30 +0100

    verify token handles the errors

--- src/app/api/verifyToken.js
+++ src/app/api/verifyToken.js
@@ -1,6 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 export function verifyToken(token) {
-    const verified = jwt.verify(token, process.env.JWT_SECRET ? process.env.JWT_SECRET : 'BackupKey');
-    return verified.userId;
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET ? process.env.JWT_SECRET : 'BackupKey');
+        return verified.userId;
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            throw new Error('Token Expired');
+        } else {
+            throw new Error('Invalid token');
+        }
+    }
 }
