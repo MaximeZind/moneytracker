10ff05fff0b65eac943c41c43a17e6e04a488a14
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-02-06 21:58:57 +0100

    getTransactions going through customFetch

--- src/app/services/getTransactions.ts
+++ src/app/services/getTransactions.ts
@@ -1,12 +1,15 @@
+import { customFetch } from "./customFetch";
+
 export async function getTransactions(token: string) {
 
-    const response = await fetch('../api/transactions', {
+    const response = await customFetch('../api/transactions', {
         method: 'GET',
         headers: {
             Accept: "application/json",
             'Authorization': `Bearer ${token}`,
-        }
-    });
+        },
+    })
+    
     if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error || 'Something went wrong');
