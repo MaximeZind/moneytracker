a4ee8de15a05d931d4bd20175d46a8ea0914841f
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-04-01 09:02:13 -0400

    account preview now shows the right balance

--- src/components/AccountPreview.tsx
+++ src/components/AccountPreview.tsx
@@ -3,6 +3,8 @@
 import styles from "./AccountPreview.module.css";
 import Link from "next/link";
 import { Transaction } from "@/types/global";
+import { useEffect, useState } from "react";
+import generateRecurringInstances from "../utils/transactions";
 
 interface Props {
     name: string;
@@ -10,18 +12,52 @@ interface Props {
     transactions?: Transaction[];
 }
 
-export default function AccountPreview({name, type, transactions }: Props) {
+export default function AccountPreview({ name, type, transactions }: Props) {
 
-    let amount = 0;
-    
-    transactions?.map((transaction) => {
-        if (transaction.type === "Income") {
-            amount = amount + transaction.amount;
-        } else if (transaction.type === "Expense") {
-            amount = amount - transaction.amount;
+    const [amount, setAmount] = useState(0);
+    const [transactionList, setTransactionList] = useState(transactions);
+
+    useEffect(() => {
+        function getAllTransactions(transactionItems: Transaction[]) {
+            const allTransactions = transactionItems.flatMap((transaction) => {
+                if (transaction.recurring === true && transaction.frequencyAmount && transaction.frequencyUnit && transaction.recurringEndingDate) {
+                    return [transaction, ...generateRecurringInstances(transaction)];
+                } else {
+                    return [transaction];
+                }
+            }).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+            const allTransactionsUpToToday: Transaction[] = allTransactions
+                .flatMap((transaction) => {
+                    return new Date(transaction.date) <= new Date() ? [transaction] : [];
+                });
+
+            if (allTransactionsUpToToday.length > 0) {
+                setTransactionList(allTransactionsUpToToday);
+            } else {
+                setTransactionList([]);
+            }
         }
-    })
-    const lastTransaction = transactions?.at(-1);
+        transactions && getAllTransactions(transactions);
+    }, [])
+
+    useEffect(() => {
+        transactionList && setAmount(calculateTotalAmount(transactionList));
+    }, [transactionList]);
+
+    function calculateTotalAmount(items: Transaction[]) {
+        let runningAmount = 0
+
+        items?.map((item: Transaction) => {
+            if (item.type === "income") {
+                runningAmount = runningAmount + item.amount;
+            } else if (item.type === "expense") {
+                runningAmount = runningAmount - item.amount;
+            }
+        })
+        return runningAmount;
+    }    
+
+    const lastTransaction = transactionList?.at(-1);    
     return (
         <Link href={""}>
             <div className={styles.account_preview}>
@@ -34,9 +70,9 @@ export default function AccountPreview({name, type, transactions }: Props) {
                     <div className={styles.account_preview_last_transaction}>
                         <p>Last Transaction:</p>
                         <div className={styles.account_preview_last_transactions_details}>
-                            {
-                                lastTransaction && lastTransaction.type === "Income" ?
-                                <p>+</p> : <p>-</p>
+                            {                                
+                                lastTransaction && lastTransaction.type === "income" ?
+                                    <p>+</p> : <p>-</p>
                             }
                             <p>{lastTransaction?.amount}</p>
                         </div>
