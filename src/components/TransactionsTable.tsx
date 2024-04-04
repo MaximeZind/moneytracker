d320b2516678e2cfd2a8edf1c07bceb0e5bc28be
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-04-04 13:58:51 -0400

    added the filter options on top of the transactions. Need to use the checkboxes to filter the transactions now

--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -4,15 +4,17 @@ import { useEffect, useState } from "react";
 import { getTransactions } from "../app/services/transactions";
 import Table from "../components/Table";
 import styles from "./TransactionsTable.module.css";
-import { Account, Transaction } from "@/types/global";
+import { Account, Category, Transaction } from "@/types/global";
 import generateRecurringInstances from "../utils/transactions";
 import Collapse from "./Collapse";
 import { getAccounts } from "@/app/services/accounts";
+import { getCategories } from "@/app/services/categories";
 
 export default function TransactionsTable() {
 
     const [transactions, setTransactions] = useState<Transaction[]>([]);
     const [accounts, setAccounts] = useState<Account[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
         const getProfile = async () => {
@@ -34,12 +36,17 @@ export default function TransactionsTable() {
                 setAccounts(response);
             })
         }
+
+        const fetchCategories = async () => {
+            await getCategories().then((response) => {
+                setCategories(response);
+            })
+        }
+
         getProfile();
         fetchAccount();
+        fetchCategories();
     }, []);
-
-    console.log(accounts);
-    
 
     // Creating datas for the table component
     const tableHeaders = ["Month", "Date", "Description", "Category", "Income", "Debit", "Balance"];
@@ -63,15 +70,53 @@ export default function TransactionsTable() {
 
     return (
         <section className={styles.transactions_section}>
-            <Collapse title="Accounts">
-                {
-                    accounts.map((account) => {
-                        return (
-                            <p key={account.id}>{account.name}</p>
-                        )
-                    })
-                }
-            </Collapse>
+            <div className={styles.transactions_section_filters}>
+                <Collapse title="Accounts">
+                    {
+                        accounts.map((account) => {
+                            return (
+                                <div key={account.id} className={styles.account_option}>
+                                    <input type="checkbox" id={account.name} name={account.name} value="true" />
+                                    <label htmlFor={account.name}>{account.name}</label>
+                                </div>
+                            )
+                        })
+                    }
+                </Collapse>
+                <Collapse title="Categories">
+                    {
+                        categories.map((category) => {
+                            return (
+                                <div key={category.id} className={styles.category_option}>
+                                    <input type="checkbox" id={category.name} name={category.name} value="true" />
+                                    <label htmlFor={category.name}>{category.name}</label>
+                                </div>
+                            )
+                        })
+                    }
+                </Collapse>
+                <Collapse title="Transactions">
+                    <p>Types:</p>
+                    <div className={styles.transaction_type_option}>
+                        <input type="checkbox" id="income" name="income" value="true" />
+                        <label htmlFor="income">Income</label>
+                    </div>
+                    <div className={styles.transaction_type_option}>
+                        <input type="checkbox" id="debit" name="debit" value="true" />
+                        <label htmlFor="debit">Debit</label>
+                    </div>
+                </Collapse>
+                <Collapse title="Dates">
+                    <div className={styles.dates_from}>
+                        <label htmlFor="dateFrom">Transactions from </label>
+                        <input type="date" name="dateFrom" id="dateFrom" className={styles.date_input} />
+                    </div>
+                    <div className={styles.dates_until}>
+                        <label htmlFor="dateUntil">Until </label>
+                        <input type="date" name="dateUntil" id="dateUntil" className={styles.date_input} />
+                    </div>
+                </Collapse>
+            </div>
             <Table headers={tableHeaders} data={tableData} />
         </section>
     )
