76c3285f2ed4062f37b8a5cff720228a97c307c2
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-04-18 19:48:00 -0400

    created the patch method for a transaction, and updated the get method

--- src/app/(loggedin)/dashboard/transactions/edit/[id]/page.tsx
+++ src/app/(loggedin)/dashboard/transactions/edit/[id]/page.tsx
@@ -1,12 +1,10 @@
 import styles from "./AddTransaction.module.css";
-import NewTransactionForm from "@/components/forms/NewTransactionForm";
 
 export default function AddTransaction() {
 
   return (
     <section className={styles.new_transaction_section}>
-      <h1>Add Transaction</h1>
-      <NewTransactionForm />
+      <h1>Edit Transaction</h1>
     </section>
   )
 }
--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -1,5 +1,3 @@
-// import { PrismaClient } from '@prisma/client';
-// import { verifyToken } from '../../verifyToken';
 import { PrismaClient } from '@prisma/client';
 import { verifyToken } from '../../verifyToken';
 import { cookies } from "next/headers";
@@ -10,30 +8,55 @@ require('dotenv').config();
 
 const prisma = new PrismaClient();
 interface CustomResponse {
-    data?: Transaction[];
+    data?: Transaction | Transaction[];
     status?: number;
     message?: string;
 }
 
-
 // Get Transaction by ID
 export async function GET(request: Request, context: any) {
+    let response: CustomResponse = {};
     const { params } = context;
     const transactionId = params.id
-    const req = await request.headers.get('authorization');
-    const token = req ? req.replace('Bearer ', '') : null;
+    const cookieStore = cookies();
+    const token = cookieStore.get(COOKIE_NAME);
     if (!token) {
         return Response.json({ error: 'Unauthorized - Token missing' });
     }
-    const userId = verifyToken(token);
-    if (userId) {
-        const transaction = await prisma.transaction.findUnique({
-            where: {
-                id: transactionId
-            },
-        })
-        return Response.json(transaction);
+    try {
+        const userId = verifyToken(token);
+        if (userId) {
+            const transaction = await prisma.transaction.findUnique({
+                where: {
+                    id: transactionId
+                },
+                include: {
+                    category: true,
+                },
+            });
+            if (transaction) {
+                response.status = 200;
+                response.data = transaction;
+            } else if (!transaction) {
+                response.status = 404;
+                response.message = 'Transaction not found';
+                response.data = [];
+            }
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            if (error.name === 'TokenExpiredError') {
+                response.status = 401;
+                response.message = 'Token Expired';
+                response.data = [];
+            } else {
+                response.status = 401;
+                response.message = 'Unauthorized';
+                response.data = [];
+            }
+        }
     }
+    return NextResponse.json({ response: response }, { status: response.status });
 }
 
 export async function DELETE(request: Request, context: any) {
@@ -93,3 +116,57 @@ export async function DELETE(request: Request, context: any) {
     }
     return NextResponse.json({ response: response }, { status: response.status });
 }
+
+export async function PATCH(request: Request, context: any) {
+    let response: CustomResponse = {};
+    const datas = await request.json();
+    const { params } = context;
+    const transactionId = params.id;
+    const cookieStore = cookies();
+    const token = cookieStore.get(COOKIE_NAME);
+    if (!token) {
+        return Response.json({ error: 'Unauthorized - Token missing' });
+    }
+    try {
+        const userId = verifyToken(token.value);
+        if (userId) {
+            const updatedTransaction = await prisma.transaction.update({
+                where: {
+                    id: transactionId
+                },
+                data: {
+                    accountId: datas.accountId,
+                    amount: datas.amount,
+                    categoryId: datas.categoryId,
+                    date: datas.date,
+                    description: datas.description,
+                    frequencyAmount: datas.frequencyAmount,
+                    frequencyUnit: datas.frequencyUnit,
+                    recurring: datas.recurring,
+                    recurringEndingDate: datas.recurringEndingDate,
+                    type: datas.type,
+                },
+                include: {
+                    category: true,
+                },
+            });
+        
+            response.status = 200;
+            response.data = [updatedTransaction];
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            if (error.name === 'TokenExpiredError') {
+                response.status = 401;
+                response.message = 'Token Expired';
+                response.data = [];
+            } else {
+                response.status = 401;
+                response.message = 'Unauthorized';
+                response.data = [];
+            }
+        }
+    }
+    return NextResponse.json({ response: response }, { status: response.status });
+}
+
