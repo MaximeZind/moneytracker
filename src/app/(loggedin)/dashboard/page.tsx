a31f8e4756bb4814f9df3418ddb4dd05e9142ac9
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-05-12 20:16:29 -0400

    the animation works correctly and displays a message according to how close the user is to their set goal

--- src/app/(loggedin)/dashboard/page.tsx
+++ src/app/(loggedin)/dashboard/page.tsx
@@ -1,19 +1,119 @@
+import { cookies } from "next/headers";
 import styles from "./Dashboard.module.css";
 import SpaceshipAndMoon from '@/components/SpaceshipAndMoon';
+import { COOKIE_NAME } from "@/constants";
+import { Transaction } from "@/types/global";
+import generateRecurringInstances from "@/utils/transactions";
+import { revalidatePath } from "next/cache";
 
 
-export default function Dashboard() {
+export default async function Dashboard() {
+
+  revalidatePath('/', 'layout');
+
+  // Data fetching
+  const transactionsData = await getTransactions();
+  const transactions = transactionsData.response.data;
+  const categoriesData = await getCategories();
+  const categories = categoriesData.response.data;
+  const settingsData = await getSettingsDatas();
+  const user = settingsData.response.data; 
+
+  // Sorting transactions
+  const transactionsList = sortTransactions(transactions);
+  const todayBalance = calculateTodaysBalance(transactionsList);
+
+  const amountGoal = user.settings.amountGoal;  
+
+  function sortTransactions(transactions: Transaction[]) {
+    const sortedTransactions = transactions.flatMap((transaction) => {
+      if (transaction.recurring === true && transaction.frequencyAmount && transaction.frequencyUnit && transaction.recurringEndingDate) {
+        return [transaction, ...generateRecurringInstances(transaction)];
+      } else {
+        return [transaction];
+      }
+    }).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    return sortedTransactions;
+  }
+
+  function calculateTodaysBalance(transactions: Transaction[]) {
+    let balance = 0;
+    const currentDate = new Date();
+    transactions.map((transaction) => {
+      // Only up until today
+      if (new Date(transaction.date) <= currentDate) {
+        if (transaction.type === 'expense') {
+          balance = balance - transaction.amount;
+        } else if (transaction.type === 'income') {
+          balance = balance + transaction.amount;
+        }
+      }
+    });
+    return balance;
+  }
+
 
 
   return (
     <>
-      {/* <h1>Dashboard</h1> */}
       <div className={styles.dashboard_layout}>
         <section className={styles.dashboard_graphs}>
 
         </section>
-        <SpaceshipAndMoon />
+        <SpaceshipAndMoon balance={todayBalance} goal={amountGoal} />
       </div>
     </>
   )
+}
+
+const getTransactions = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get(COOKIE_NAME);
+  if (token) {
+    const res = await fetch(`${process.env.API_BASE_URL}api/transactions`, {
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        Authorization: 'Bearer ' + token.value,
+      },
+    })
+    return res.json();
+  } else if (!token) {
+    return
+  }
+}
+
+const getCategories = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get(COOKIE_NAME);
+  if (token) {
+    const res = await fetch(`${process.env.API_BASE_URL}api/categories`, {
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        Authorization: 'Bearer ' + token.value,
+      },
+    })
+    return res.json();
+  } else if (!token) {
+    return
+  }
+}
+
+const getSettingsDatas = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get(COOKIE_NAME);
+  if (token) {
+    const res = await fetch(`${process.env.API_BASE_URL}api/settings`, {
+      next: { revalidate: 10 },
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        Authorization: 'Bearer ' + token.value,
+      },
+    })
+    return res.json()
+  } else if (!token) {
+    return
+  }
 }
--- src/components/SpaceshipAndMoon.tsx
+++ src/components/SpaceshipAndMoon.tsx
@@ -1,15 +1,112 @@
+'use client'
+
 import Image from 'next/image';
 import styles from './SpaceshipAndMoon.module.css';
 import spaceship_on from "@/app/assets/spaceship_on.png";
 import spaceship_off from "@/app/assets/spaceship_off.png";
 import moon from '@/app/assets/moon.png';
+import { useEffect, useState } from 'react';
+
+interface SpaceshipAndMoonProps {
+    balance: number;
+    goal: number;
+}
+
+export default function SpaceshipAndMoon({ balance, goal }: SpaceshipAndMoonProps) {
+
+    const percentage = (balance * 100 / goal) / 100;
+    const [isSpaceshipOn, setIsSpaceshipOn] = useState(false);
+    const [message, setMessage] = useState('');
 
 
-export default function HeroSection() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsSpaceshipOn(true);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage(setUpMessage(percentage));
+        }, 7000);
+
+        return () => clearTimeout(timer);
+    }, [percentage]);
+
+    function setUpMessage(percentage: number) {
+        if (percentage < 0.2) {
+            return "Keep going! You're just getting started.";
+        } else if ((percentage >= 0.2) && (percentage < 0.3)) {
+            return "Making progress! You're on the right track.";
+        } else if ((percentage >= 0.3) && (percentage < 0.4)) {
+            return "Getting closer! Keep up the momentum.";
+        } else if ((percentage >= 0.4) && (percentage < 0.5)) {
+            return "Halfway there! Your efforts are paying off.";
+        } else if ((percentage >= 0.5) && (percentage < 0.6)) {
+            return "Progressing well! Keep pushing forward.";
+        } else if ((percentage >= 0.6) && (percentage < 0.7)) {
+            return "Almost there! Stay focused on your goal.";
+        } else if ((percentage >= 0.7) && (percentage < 0.8)) {
+            return "Great job! You're nearing the finish line.";
+        } else if ((percentage >= 0.8) && (percentage < 0.9)) {
+            return "Almost achieved! Keep the determination.";
+        } else if ((percentage >= 0.9) && (percentage < 1)) {
+            return "You're so close! Keep going for the win.";
+        } else if (percentage >= 1) {
+            return "Congratulations! Goal achieved, time for celebration!";
+        }
+        return "";
+    }
+
+    function setSpaceshipClassname(percentage: number) {
+        if ((percentage >= 0) && (percentage < 0.2)) {
+            return `${styles.spaceship_10}`;
+        } else if ((percentage >= 0.2) && (percentage < 0.3)) {
+            return `${styles.spaceship_20}`;
+        } else if ((percentage >= 0.3) && (percentage < 0.4)) {
+            return `${styles.spaceship_30}`;
+        } else if ((percentage >= 0.4) && (percentage < 0.5)) {
+            return `${styles.spaceship_40}`;
+        } else if ((percentage >= 0.5) && (percentage < 0.6)) {
+            return `${styles.spaceship_50}`;
+        } else if ((percentage >= 0.6) && (percentage < 0.7)) {
+            return `${styles.spaceship_60}`;
+        } else if ((percentage >= 0.7) && (percentage < 0.8)) {
+            return `${styles.spaceship_70}`;
+        } else if ((percentage >= 0.8) && (percentage < 0.9)) {
+            return `${styles.spaceship_80}`;
+        } else if ((percentage >= 0.9) && (percentage < 1)) {
+            return `${styles.spaceship_90}`;
+        } else if (percentage >= 1) {
+            return `${styles.spaceship_100}`;
+        }
+    }
+
     return (
         <div className={styles.spaceship_and_moon}>
-            <Image className={styles.moon} src={moon} alt={'moon'} />
-            <Image className={styles.spaceship} src={spaceship_off} alt={'spaceship'} />
+            <Image
+                className={styles.moon}
+                src={moon}
+                alt={'moon'} />
+            <span className={`${setSpaceshipClassname(percentage)} ${styles.spaceship}`}>
+                {
+                    isSpaceshipOn ?
+                        <Image
+                            src={spaceship_on}
+                            alt={'spaceship'} /> :
+                        <Image
+                            src={spaceship_off}
+                            alt={'spaceship'} />
+                }
+            </span>
+            {
+                message !== '' && 
+                <div className={styles.message_box}>
+                    <p>{message}</p>
+                </div>
+            }
         </div>
     )
 }
