6c35e320ba9d6aa24f7ec11043c9f4e04484e0b6
Maxime Zinderstein <maximezinderstein@gmail.com>
2024-05-30 16:31:55 -0400

    the navigation menu open and closes correctly on mobile

--- src/components/baselayout/LoggedInHeader.tsx
+++ src/components/baselayout/LoggedInHeader.tsx
@@ -3,6 +3,7 @@ import styles from './LoggedInHeader.module.css';
 import Button from '@/components/forms/formscomponents/SubmitButton';
 import { signOut } from '@/app/services/signout';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 
 export default function LoggedInHeader() {
@@ -12,10 +13,17 @@ export default function LoggedInHeader() {
         signOut();
         router.push('/login');
     }
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <header className={styles.header}>
-            <nav className={styles.header_nav}>
+            <div className={isOpen ? `${styles.burger_menu} ${styles.open}` : `${styles.burger_menu}`}
+                onClick={() => setIsOpen(!isOpen)}>
+                <span className={`${styles.burger_menu_bar} ${styles.top_bar}`}></span>
+                <span className={`${styles.burger_menu_bar} ${styles.middle_bar}`}></span>
+                <span className={`${styles.burger_menu_bar} ${styles.bottom_bar}`}></span>
+            </div>
+            <nav className={isOpen ? `${styles.header_nav} ${styles.open}` : `${styles.header_nav}`}>
                 <Button url='/dashboard' value='' text='Dashboard' />
                 <Button url='/dashboard/transactions' value='' text='Transactions' />
                 <Button url='/dashboard/accounts' value='' text='Accounts' />
